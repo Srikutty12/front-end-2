d70ebcfba00f407f176d51b4ccf4cb5f488e3024
sri <kuttysri2002jgmail.com>
2022-12-11 11:53:23 +0530

    js intro

---
+++ javascript/script.js
@@ -0,0 +1,30 @@
+
+console.log(1567);
+alert(78 + 2784);
+var age = '20';
+age = "21";
+console.log(age);
+
+let firstName = "Sri";
+firstName = "Sri";
+console.log(firstName);
+
+const gender = "Female";
+
+// gender = "male" // We should not reassign const variable
+console.log(gender)
+
+let lastName = "Kutty";
+
+// String concatination
+
+let fullName = firstName + ' ' + lastName;  
+
+
+console.log(fullName);
+
+// Case sensitive
+const Gender = ""
+
+
+

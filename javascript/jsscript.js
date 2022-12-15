bdc564e344b38adff0d5abf42b5672399085c2fe
sri <kuttysri2002jgmail.com>
2022-12-15 20:10:32 +0530

    data type

---
+++ javascript/jsscript.js
@@ -0,0 +1,18 @@
+let firstName = 'Sri'; 
+firstName = "Sri"
+let age = 20; 
+let isMale = true; 
+let dob; 
+let address = null;
+
+console.log(firstName, "String Type");
+console.log(age, "Number Type");
+console.log(isMale, "Boolean Type");
+console.log(dob, "Undefined Type");
+console.log(address, "Null Type");
+
+console.log(typeof firstName, "String Type");
+console.log(typeof age, "Number Type");
+console.log(typeof isMale, "Boolean Type");
+console.log(typeof dob, "Undefined Type");
+console.log(typeof address, "Null Type");
